9da2354a4483fb8bd5bea81db84967e37c183c35
ghostry <ghostry@ghostry.cn>
2019-05-05 11:11:57 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ if [ ! -L dl ];then
     mkdir -p /data/build_dir
     ln -s /data/build_dir ./
     mv /home/op/openwrt/staging_dir /data/staging_dir
+    rm -rf /home/op/openwrt/staging_dir
     mkdir -p /data/staging_dir
     ln -s /data/staging_dir ./
     chmod -R 777 /data
